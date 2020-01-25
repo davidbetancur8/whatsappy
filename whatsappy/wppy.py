b58b35cffa8a89726567dd86869abb09533a3e2a
davidbetancur8 <david.bet.san@gmail.com>
2020-01-25 17:28:25 -0500

    clouds

--- whatsappy/wppy.py
+++ whatsappy/wppy.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 import re
+import collections
+from stop_words import get_stop_words
+from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 plt.style.use("fivethirtyeight")
 
@@ -44,4 +47,30 @@ def plot_word(df, palabra, max_count):
 def plot_count(df):
     df_count = df['name'].value_counts()
     df_count.head(10).plot.bar(figsize=(5, 5))
+    plt.show()
+
+
+def agrupar(x):
+    todo = []
+    for ex in x:
+        todo.append(ex)
+    todo_str = " ".join(todo)
+    return todo_str
+
+
+def general_cloud(df):
+    all_text = df.groupby([True]*len(df)).message.apply(agrupar).values[0]
+    stopwords = get_stop_words('es')
+    lista_propia = ["<multimedia", "omitido>"]
+    for palabra in lista_propia:
+        stopwords.append(palabra)
+    wordcount = collections.defaultdict(int)
+    for word in all_text.lower().split():
+        if word not in stopwords:
+            wordcount[word] += 1
+    
+    wc = WordCloud(background_color="white",width=1000, height=1500).generate_from_frequencies(wordcount)
+    fig = plt.figure(figsize=(15,15))
+    plt.imshow(wc, interpolation="bilinear")
+    plt.axis("off")
     plt.show()
